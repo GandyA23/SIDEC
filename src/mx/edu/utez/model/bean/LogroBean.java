36321e0b65845a912a4b21eba1a4758bb284bf9f
GandyA23 <avila.gandy@gmail.com>
2020-07-06 19:42:39 -0500

    LogroBean Actualizado

--- src/mx/edu/utez/model/bean/LogroBean.java
+++ src/mx/edu/utez/model/bean/LogroBean.java
@@ -1,22 +1,53 @@
 package mx.edu.utez.model.bean;
 
+import java.util.Date;
+
 public class LogroBean {
-    private int id;
-    private String nombre;
 
-    public int getId() {
-        return id;
+    private EstudianteBean estudiante = new EstudianteBean();
+
+    private int folio;
+    private String tipo;
+    private String descripcion;
+    private Date fecha ;
+
+    public EstudianteBean getEstudiante() {
+        return estudiante;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setEstudiante(EstudianteBean estudiante) {
+        this.estudiante = estudiante;
     }
 
-    public String getNombre() {
-        return nombre;
+    public String getTipo() {
+        return tipo;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getFolio() {
+        return folio;
+    }
+
+    public void setFolio(int folio) {
+        this.folio = folio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
     }
 }
