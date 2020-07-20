4413ee3ac9c356eb8ed503148227e51c62c4b5dd
JairDavid <20193tn071@utez.edu.mx>
2020-07-19 22:51:39 -0500

    Se agregó funciones al dao de información personal

--- src/mx/edu/utez/model/dao/Informacion_PersonalDao.java
+++ src/mx/edu/utez/model/dao/Informacion_PersonalDao.java
@@ -1,4 +1,18 @@
 package mx.edu.utez.model.dao;
 
 public class Informacion_PersonalDao {
+    public static String insertarDatos(){
+        return "1";
+    }
+    public static String eliminarDatos(){
+        return "1";
+    }
+    public static String buscarDatos(){
+        return "1";
+
+
+    }
+    public static String modificarDatos(){
+       return "1";
+    }
 }
